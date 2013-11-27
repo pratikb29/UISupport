973133c39be4ea2a598a7d2e9072634858af5bc5
gwwindia <gwwprogrammer@gmail.com>
2013-11-27 11:51:06 +0530

    commit 3

--- UISupport/AppDelegate.h
+++ UISupport/AppDelegate.h
@@ -3,7 +3,7 @@
 //  UISupport
 //
 //    Created by Yogesh Tank on 27/11/13.
-//  Copyright (c) 2013 Yogesh Tank. All rights reserved.
+//    Copyright (c) 2013 Yogesh Tank. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
